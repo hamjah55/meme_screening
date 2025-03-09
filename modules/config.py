51359c4a7bb132d4ecba23e65df3b35f9060bfc1
hamjah55 <132764873+hamjah55@users.noreply.github.com>
2025-03-09 22:12:56 +0700

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,4 +1,1 @@
-# Konfigurasi API & Token Telegram
-DEXSCREENER_API_URL = "https://api.dexscreener.com/latest/dex/pairs/solana"
-TELEGRAM_BOT_TOKEN = "8035472871:AAG0eSaWi5nSXtVv368bRBPnNy9H1X0iEJs"
-TELEGRAM_CHAT_ID = "1107052040"
+
